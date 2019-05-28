3d769bc17307c37077b017d844854e68bdec0bef
ArtemGum <artemgumarov840@gmail.com>
2019-05-28 09:21:51 +0500

    update

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<newPostForm
+	:postEdit="post"
+	@submit="onSubmit"/>
+</template>
+
+<script>
+import newPostForm from '@/components/Admin/newPostForm.vue'
+export default {
+	components: { newPostForm },
+	layout: 'admin',
+	data () {
+		return {
+			post: {
+				id: 1,
+				title: '1 post',
+				descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+				content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+				img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg'
+			}
+		}
+	},
+	methods: {
+		onSubmit (post) {
+			console.log('Post Editing!')
+			console.log(post)
+		}
+	}
+}
+</script>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<newPostForm @submit="onSubmit"/>
+</template>
+
+<script>
+import newPostForm from '@/components/Admin/newPostForm.vue'
+export default {
+	components: { newPostForm },
+	layout: 'admin',
+	methods: {
+		onSubmit (post) {
+			console.log('Post added')
+			console.log(post)
+		}
+	}
+}
+</script>
